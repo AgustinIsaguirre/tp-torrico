c23f5d1756e8fca189e649df161b34310bbf7eaa
agustin isaguirre <agusto090@hotmail.com>
2021-05-30 15:41:29 -0300

    Ejercicio1 terminado

--- ejercicio1.js
+++ ejercicio1.js
@@ -1,33 +1,67 @@
+let nota1=undefined;
+let corte=undefined;   
+do{    
+        do{
+        
+            nota1 = window.prompt("Ingrese una nota del 0 al 10");
 
-do{  
-    let nota1 = window.prompt("Ingrese una nota del 0 al 10");
+            nota1 = parseInt(nota1);
 
-    nota1 = parseInt(nota1);
-}
-while(isNaN(nota1));
+            console.log(nota1);
 
-do{
-
-
-
-        switch(nota1){
-
-            case 0,1,2,3:{console.log("Muy deficiente");break;}
-
-            case 4,5:{console.log("insuficiente");break;}
-
-            case 6:{console.log("suficiente");break;}
-
-            case 7:{console.log("Bien");break;}
-
-            case 8,9:{console.log("Notable");break;}
-
-            case 10:{console.log("Sobresaliente");break;} 
-
-            default:{console.log("Debe ingresar un numero entre 0 y 10");break;}
         }
 
+        while(isNaN(nota1));
+
+        if(nota1<0 || nota1>10){
+
+            alert("Debe Ingresar un número entre 0 y 10");
+        
+            do{
+
+                nota1 = window.prompt("Ingrese una nota del 0 al 10");
+            
+                nota1 = parseInt(nota1);
+            
+            }
+            
+            while(isNaN(nota1));
+        }
+        
+    switch(nota1){
+
+                case 0:
+                case 1:
+                case 2:
+                case 3:{alert("Muy deficiente");break;}
+
+                case 4:
+                case 5:{alert("insuficiente");break;}
+
+                case 6:{alert("suficiente");break;}
+
+                case 7:{alert("Bien");break;}
+
+                case 8:
+                case 9:{alert("Notable");break;}
+
+                case 10:{alert("Sobresaliente");break;} 
+          
+               
+            
+     }
+     corte = window.confirm("¿Desea ingresar otra nota?");
+}   
+      while(corte===true);
+    
+
+
+
+
     
     
-}
-while(nota1 <0 || nota1 >10);
+    
+    
+
+
+
